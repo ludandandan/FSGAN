bb54c8ed64697e84d90cfcaa0c1b4eeb89eed9dc
ludandandan <ludandandan1994@gmail.com>
2023-12-29 21:07:12 +0800

    模型测试的代码

--- single_model_test.sh
+++ single_model_test.sh
@@ -4,4 +4,5 @@ python test.py --checkpoints_dir checkpoints --name ckpt_${exp_id} \
 --model test --dataset_mode aligned --norm batch --use_local \
 --which_epoch ${ep} --results_dir dataset/celeba_hq_256/results \
 --dataroot dataset/celeba_hq_256/photo --data_json dataset/celeba_hq_256/celeba.json \
---bg_dir dataset/celeba_hq_256/mask
+--bg_dir dataset/celeba_hq_256/mask --gpu_ids 1
+
